f153c6144912054dd29c389d199ff817ead2b947
Leonardo <leomartins.cavalheiro@gmail.com>
2024-06-17 10:01:51 -0300

    Projeto DIO Banco Digital

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,14 @@
+public class ContaPoupanca extends Conta {
+        public ContaPoupanca(Cliente cliente) {
+            super(cliente);
+        }
+
+        public ContaPoupanca() {  //construtor sugerido pela IDE
+            super();
+        }
+
+        public void imprimirExtrato() {
+            System.out.println("#### Extrato de Conta Poupança #####");
+            super.imprimirInfosComuns();
+        }
+}
